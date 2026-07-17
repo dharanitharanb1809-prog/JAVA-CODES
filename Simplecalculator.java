41b31c3f7f2fb47ef3c52f885b738c67a5f65b39
dharanitharanb1809-prog <dharanitharanb1809@gmail.com>
2026-07-17 15:33:56 +0530

    Implement basic operations in Simplecalculator

---
+++ Simplecalculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+          System.out.println("enter first num:");
+          int a =sc.nextInt();
+          System.out.println("enter second num");
+          int b =sc.nextInt();
+          System.out.println("addition="+(a+b));
+           System.out.println("subtraction="+(a-b));
+            System.out.println("multiplication="+(a*b));
+             System.out.println("division="+(a/b));
+    }
+}
