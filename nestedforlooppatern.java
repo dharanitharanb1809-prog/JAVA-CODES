c8b7d38971ea59e4e97430565a2a619162b21368
dharanitharanb1809-prog <dharanitharanb1809@gmail.com>
2026-07-20 06:51:48 +0530

    Create nestedforlooppatern.java

---
+++ nestedforlooppatern.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+        int m=10;
+        pat(n);
+       
+    }
+        static void pat(int k)
+        {
+            for(int i=1;i<k;i++)
+        {
+            for(int j=i;j<k;j++)
+            
+            {
+                 System.out.print(j+"  ");
+        }
+        System.out.println();
+       
+        }
+        }
+        }
