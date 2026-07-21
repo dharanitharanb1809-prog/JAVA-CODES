83f3275018b6b727ead7c618b01b9685991343ef
dharanitharanb1809-prog <dharanitharanb1809@gmail.com>
2026-07-21 14:48:21 +0530

    Add Main class to display subject marks

---
+++ Arraymarks.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main (String[]args){
+        int[]marks={10,20,30,40,50};
+        String[]subject={"Tamil","English","Maths","Science","Social"};
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println(subject[i]+"="+marks[i]);
+        }
+    }
+}
